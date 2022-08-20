8f6be564f3609303cd6adf0fb460467187e299a0
SimonTaurus <simon.stier@gmx.de>
2022-08-20 08:13:48 +0200

    implement minimal treejs version

--- src/editors/tree.js
+++ src/editors/tree.js
@@ -1,23 +1,24 @@
 /*
 
 Creates a tree view to select a value.
+Based on https://www.jstree.com/
 
 */
 import { StringEditor } from './string.js'
 import { extend } from '../utilities.js'
 
-export class ColorEditor extends StringEditor {
+export class TreeEditor extends StringEditor {
   postBuild () {
-    if (window.Picker) {
-      this.input.type = 'text'
-    }
-    this.input.style.padding = '3px'
+    window.document.addEventListener('click', (event) => {
+      if (!this.container.contains(event.target)) this.setVisible(false)
+      else this.setVisible(true)
+    })
   }
 
   setValue (value, initial, fromTemplate) {
     const res = super.setValue(value, initial, fromTemplate)
-    if (this.picker_instance && this.picker_instance.domElement && res && res.changed) {
-      this.picker_instance.setColor(res.value, true)
+    if (this.tree && this.treeContainer && res && res.changed) {
+      this.tree.jstree(true).select_node(res.value)
     }
     return res
   }
@@ -28,74 +29,52 @@ export class ColorEditor extends StringEditor {
 
   afterInputReady () {
     super.afterInputReady()
-    this.createPicker(true)
+    this.createTree(true)
   }
 
   disable () {
     super.disable()
-    if (this.picker_instance && this.picker_instance.domElement) {
-      /* Disable picker cursor dragging */
-      this.picker_instance.domElement.style.pointerEvents = 'none'
-      /* Disable picker buttons */
-      const buttons = this.picker_instance.domElement.querySelectorAll('button')
-      for (let i = 0; i < buttons.length; i++) {
-        buttons[i].disabled = true
-      }
-    }
   }
 
   enable () {
     super.enable()
-    if (this.picker_instance && this.picker_instance.domElement) {
-      /* Enable picker cursor dragging */
-      this.picker_instance.domElement.style.pointerEvents = 'auto'
-      /* Enable picker buttons */
-      const buttons = this.picker_instance.domElement.querySelectorAll('button')
-      for (let i = 0; i < buttons.length; i++) {
-        buttons[i].disabled = false
-      }
-    }
   }
 
   destroy () {
-    this.createPicker(false)
     super.destroy()
+    window.jQuery(this.treeContainer.id).jstree.destroy()
   }
 
   /* helper functions */
-  createPicker (create) {
-    if (create) { /* create vanilla-picker */
-      if (window.Picker && !this.picker_instance) { /* do when vanilla-picker loaded */
-        const options = this.expandCallbacks('colorpicker', extend({}, {
-          editor: false, /* default no editor */
-          alpha: false, /* default no alpha */
-          color: this.value,
-          popup: 'bottom' /* show in the bottom */
-        }, this.defaults.options.colorpicker || {}, this.options.colorpicker || {}, {
-          parent: this.container
-        }))
+  createTree (create) {
+    // const editor = this
 
-        const updateHandler = color => {
-          const format = this.picker_instance.settings.editorFormat
-          const isAlpha = this.picker_instance.settings.alpha
-          this.setValue(format === 'hex' ? (isAlpha ? color.hex : color.hex.slice(0, 7)) : color[`${format + (isAlpha ? 'a' : '')}String`])
-        }
-        if (!options.popup && typeof options.onChange !== 'function') options.onChange = updateHandler
-        else if (options.popup && typeof options.onDone !== 'function') options.onDone = updateHandler
+    /* fetch config from schema */
+    this.options.tree = this.expandCallbacks('tree', extend({}, {
+      editor: 'jstree', /* default use jstree */
+      jstree: { data: [] }, /* default no data */
+      value: 'text', /* store the nodes text attribute */
+      mode: 'inline', /* show inline (no popup) */
+      position: 'bottom' /* show in the bottom */
+    }, this.defaults.options.tree || {}, this.options.tree || {}, {
+      parent: this.container
+    }))
 
-        this.picker_instance = new window.Picker(options)
-        /* this.picker_instance.openHandler() */
-        if (!options.popup) { /* use inline colorPicker */
-          this.input.style.display = 'none'
-          this.theme.afterInputReady(this.picker_instance.domElement)
-        }
-      }
-    } else { /* destroy vanilla-picker */
-      if (this.picker_instance) {
-        this.picker_instance.destroy()
-        this.picker_instance = null
-        this.input.style.display = ''
-      }
-    }
+    this.treeContainer = document.createElement('div')
+    this.treeContainer.id = 'tree-' + this.container.dataset.schemapath.replaceAll('.', '-') /* jstree / jQuery doesn't like dots and hyphens within the id */
+    this.setVisible(false)
+    this.container.appendChild(this.treeContainer)
+
+    this.tree = window.jQuery('#' + this.treeContainer.id).jstree(this.options.tree.jstree)
+    this.tree.on('changed.jstree', (event, data) => {
+      /* eslint-disable-next-line no-console */
+      console.log(this)
+      this.setValue(data.node[this.options.tree.value])
+    })
+  }
+
+  setVisible (visible) {
+    if (visible) this.treeContainer.style.display = 'block'
+    else this.treeContainer.style.display = 'none'
   }
 }
