c0ed5d484ab0034a5df382fc42296cae0400eea5
anaprikho <anastasia.prikhodina@gmail.com>
2022-08-25 12:37:37 +0200

    show tree inside popup onclick

--- src/editors/tree.js
+++ src/editors/tree.js
@@ -8,14 +8,41 @@ import { StringEditor } from './string.js'
 import { extend } from '../utilities.js'
 
 export class TreeEditor extends StringEditor {
-  postBuild () {
-    window.document.addEventListener('click', (event) => {
-      if (!this.container.contains(event.target)) this.setVisible(false)
-      else this.setVisible(true)
+  postBuild () { /* 2. after getNumCols() */
+    /* eslint-disable-next-line no-console */
+    console.log('in postBuild fct')
+    /*
+    div
+    */
+    this.popupContainer = document.createElement('div')
+    this.popupContainer.id = 'tree-' + this.container.dataset.schemapath.replaceAll('.', '-') /* jstree / jQuery doesn't like dots and hyphens within the id */
+    /* eslint-disable-next-line no-console */
+    console.log('this.popupContainer.id: ', this.popupContainer.id)
+    this.container.appendChild(this.popupContainer)
+    /*
+    create a popup dialog and show on input field click
+    */
+    window.jQuery('#' + this.popupContainer.id).dialog({
+      title: 'Tree Structure : ' + this.popupContainer.id,
+      height: 400,
+      width: 400,
+      autoOpen: false,
+      modal: true,
+      buttons: {
+        OK: function () {},
+        Cancel: function () { window.jQuery(this).dialog('close') }
+      }
+    })
+    this.input.addEventListener('click', (event) => {
+      /* eslint-disable-next-line no-console */
+      console.log('input field eventListener : ', this.container.dataset.schemapath)
+      window.jQuery('#' + this.popupContainer.id).dialog('open')
     })
   }
 
-  setValue (value, initial, fromTemplate) {
+  setValue (value, initial, fromTemplate) { /*  3. after getNumCols() -> postBuild() */
+    /* eslint-disable-next-line no-console */
+    console.log('in setValue fct')
     const res = super.setValue(value, initial, fromTemplate)
     if (this.tree && this.treeContainer && res && res.changed) {
       this.tree.jstree(true).select_node(res.value)
@@ -23,30 +50,42 @@ export class TreeEditor extends StringEditor {
     return res
   }
 
-  getNumColumns () {
+  getNumColumns () { /* 1. */
+    /* eslint-disable-next-line no-console */
+    console.log('in getNumColumns fct')
     return 2
   }
 
-  afterInputReady () {
+  afterInputReady () { /*  4. after getNumCols() -> postBuild() -> setValue() */
+    /* eslint-disable-next-line no-console */
+    console.log('in afterInputReady fct')
     super.afterInputReady()
     this.createTree(true)
   }
 
   disable () {
+    /* eslint-disable-next-line no-console */
+    console.log('in disable fct')
     super.disable()
   }
 
   enable () {
+    /* eslint-disable-next-line no-console */
+    console.log('in enable fct')
     super.enable()
   }
 
   destroy () {
+    /* eslint-disable-next-line no-console */
+    console.log('in destroy fct')
     super.destroy()
     window.jQuery(this.treeContainer.id).jstree.destroy()
   }
 
   /* helper functions */
   createTree (create) {
+    /* eslint-disable-next-line no-console */
+    console.log('in createTree fct')
     // const editor = this
 
     /* fetch config from schema */
@@ -59,21 +98,19 @@ export class TreeEditor extends StringEditor {
     }, this.defaults.options.tree || {}, this.options.tree || {}, {
       parent: this.container
     }))
-
+    /*
+    create a tree container inside the popup dialog
+    */
     this.treeContainer = document.createElement('div')
     this.treeContainer.id = 'tree-' + this.container.dataset.schemapath.replaceAll('.', '-') /* jstree / jQuery doesn't like dots and hyphens within the id */
-    this.setVisible(false)
-    this.container.appendChild(this.treeContainer)
-
+    this.treeContainer.innerText = 'This is a tree container ID: ' + this.treeContainer.id
+    this.popupContainer.appendChild(this.treeContainer)
     this.tree = window.jQuery('#' + this.treeContainer.id).jstree(this.options.tree.jstree)
-    this.tree.on('changed.jstree', (event, data) => {
-      /* eslint-disable-next-line no-console */
-      console.log(this)
-      this.setValue(data.node[this.options.tree.value])
-    })
   }
 
   setVisible (visible) {
+    /* eslint-disable-next-line no-console */
+    console.log('in setVisible fct: ', visible)
     if (visible) this.treeContainer.style.display = 'block'
     else this.treeContainer.style.display = 'none'
   }
